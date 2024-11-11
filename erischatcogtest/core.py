d838db4fd10bfa4b197f44c6bfe77c253967137e
lew <139905304+levrx@users.noreply.github.com>
2024-11-11 10:44:50 +0000

    Update core.py

--- erischatcogtest/core.py
+++ erischatcogtest/core.py
@@ -324,7 +324,7 @@ class Chat(commands.Cog):  # Inherit from commands.Cog
 
 
     @commands.command()
-    async def expand(self, ctx: commands.Context):
+    async def expandimage(self, ctx: commands.Context):
 
         channel: discord.abc.Messageable = ctx.channel
         message: discord.Message = ctx.message
