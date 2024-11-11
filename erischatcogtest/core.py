d935f339a59f6b161145ee02e846005445c7ce26
lew <139905304+levrx@users.noreply.github.com>
2024-11-11 10:23:49 +0000

    Update core.py

--- erischatcogtest/core.py
+++ erischatcogtest/core.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import requests
 import json
 
-class SabbyBot(commands.Cog):
+class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.tokens = None
