5bcb22e0ae67a908c1fdd6792dfe6adb2db3aab6
Sablinova <160382122+Sablinova@users.noreply.github.com>
2025-06-20 02:44:54 +0300

    dumdum fix

--- pstreamstatus/core.py
+++ pstreamstatus/core.py
@@ -188,8 +188,8 @@ class PStreamStatus(commands.Cog):
         for region, data in raw.items():
             msg += f"**{region}**
 ```json
-
-{str(data)[:1000]}```\n"
+{json.dumps(data, indent=2)[:1000]}```
+"
         await ctx.send(msg or "No debug info available.")
 
 
